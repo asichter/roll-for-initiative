7bc7751885746f327b02b59d8e7516944e852504
Raviteja Vajja <70241397+rvajja46@users.noreply.github.com>
2022-04-18 16:27:50 -0400

    i am speeeed

--- microcontroller_subsystem/microcontroller-files/inc/ui.h
+++ microcontroller_subsystem/microcontroller-files/inc/ui.h
@@ -5,8 +5,17 @@
 
 void LCD_FontTest();
 void LCD_DisplayUIBox(u8 mode);
+void LCD_InitUI();
 void LCD_DrawCharScale(u16 x,u16 y,u16 fc, u16 bc, char num, u8 size, u16 scale);
-void LCD_DrawScale2(const Picture *pic, uint8_t scale);
+void LCD_DrawScale2(u16 x0, u16 y0, const Picture *pic, u16 color);
+void LCD_DrawScale3(u16 x0, u16 y0, const Picture *pic, u16 color);
+void LCD_DrawScale4(u16 x0, u16 y0, const Picture *pic, u16 color);
+void LCD_DrawScale8(u16 x0, u16 y0, const Picture *pic, u16 color);
+void LCD_DrawTotal(int total);
+void LCD_DrawRoll(int roll);
+void LCD_DrawSubMod();
+void LCD_DrawMod();
+void LCD_DrawDAdv();
 void printchr(u16 x,u16 y, u16 fc, const char *p, u8 elem, u8 scale);
 void printmod(int mod, u16 mode, u8 sign);
 void printdadv(const u8 p);
