8101c6cc313c0b98e8192aeb19a57a7d55b5616b
Amogh Shanbhag <54295715+ashanbha27@users.noreply.github.com>
2022-05-04 15:47:39 -0400

    Updated version for rfi

--- pi_subsystem/rfi.py
+++ pi_subsystem/rfi.py
@@ -8,7 +8,11 @@
 
 # Import Statments
 import serial
+import sys
+sys.path.append("/home/asichter/Desktop/roll-for-initiative/pi_subsystem")
 import diceRecognition
+import pdb
+import time
 
 # Signal PiCamera to take a picture
 def trigger_picam():
@@ -20,54 +24,89 @@ def process():
 
 class RFI:
     def __init__(self):
-        try:
-            self.ser = serial.Serial(
-                port="dev/ttys0",
-                baudrate=115200,
-                parity=serial.STOPBITS_ONE,
-                bytesize=serial.EIGHTBITS,
-                timeout=1)
-            self.ser.close()
-        except serial.SerialException:
-            print("Error opening serial connection")
+        test_start = time.time()
+        while(time.time() - test_start < 6):
+            try:
+                self.ser = serial.Serial(
+                    port="/dev/ttyAMA0",
+                    baudrate=115200,
+                    stopbits=serial.STOPBITS_ONE,
+                    bytesize=serial.EIGHTBITS,
+                    timeout=1,
+                    writeTimeout=0)
+                self.ser.close()
+                print("AMA0")
+                break
+            except serial.SerialException:
+                print("Error opening serial connection ttyAMA0")
+            try:
+                self.ser = serial.Serial(
+                    port="/dev/ttyS0",
+                    baudrate=115200,
+                    stopbits=serial.STOPBITS_ONE,
+                    bytesize=serial.EIGHTBITS,
+                    timeout=1,
+                    writeTimeout=0)
+                self.ser.close()
+                print("s0")
+                break
+            except serial.SerialException:
+                print("Error opening serial connection ttyS0")
         
 
 
     # Export Number Array
     def export(self, dice_array):
         ser = self.ser
-        ser.open()
+        #ser.open()
+        print('exporting\n')
 
+        if(ser.isOpen() == False):                                                                                                                         
+            ser.open() 
         for num in dice_array:
-            if ser.is_open():
+            #pdb.set_trace()
+            #print('here1')
+            if ser.isOpen():
+                #print('here2')
                 try:
-                    ser.write(serial.to_bytes(num))
+                    #ser.write(serial.to_bytes(num))
+                    #ser.write(num.encode('utf-8'))
+                    # ser.write("H".encode('utf-8'))
+                    # output = "H"
+                    ascii_out = chr(int(num+"\n"))
+                    print(ord(ascii_out))
+                    # test_str = "this is a test!"
+                    ser.write(ascii_out.encode()) 
+                    time.sleep(0.05)
                 except serial.SerialTimeoutException:
                     print("TIMEOUT: write took too long")
-
-        ser.close()
+        if ser.isOpen():
+            ser.close()
         return
 
 def main():
     # SETUP PORT
+    #pdb.set_trace()
     rfi = RFI()
-    
 
-    recv_data = False
+    if(rfi.ser.isOpen() == False):                                                                                                                         
+        rfi.ser.open() 
+    rfi.export(["65"])  
     while True:
-        rfi.ser.open()
-        # Look for USART signal from microcontroller
+        if(rfi.ser.isOpen() == False):                                                                                                                         
+            rfi.ser.open()                                                    
+        # Look for USART signal from microcontroller                                                                              
         recv_data = rfi.ser.readline()
-        if recv_data:
-            rfi.ser.close()
-            dice_pictures = trigger_picam()
-            dice_array = process(dice_pictures)
-            break
-        # Signal PiCamera to take a picture
-        # Process picture
-        # Export number array
-        rfi.export(["1"])
-        break
+        print(recv_data)
+        if recv_data: 
+            print("recv")                                                                                                                                                                                                                                                                                                                                                                                                                                  
+            dice_array = diceRecognition.process()
+            print(dice_array)
+            dice_array = [str(die) for die in dice_array]
+            rfi.export(dice_array)
+
+        # rfi.ser.close()
+        # break
 
 if __name__ == "__main__":
     main()
